260cbaa28b114add1a94230d34903cffe65319a5
Geoffrey Obwocha <ondiekobwocha@gmail.com>
2026-08-21 10:08:33 +0300

    Edited products and cors

--- backend/app/Http/Controllers/ProductController.php
+++ backend/app/Http/Controllers/ProductController.php
@@ -10,6 +10,10 @@ class ProductController extends Controller
     function index()
     {
         $products = Product::all();
+
+        foreach ($products as $product){
+            $product->image = asset('/api/image/' . $product->image);
+        }
         return response()->json($products);
     }
 }
